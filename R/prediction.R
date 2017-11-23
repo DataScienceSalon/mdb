9589846ee4d258f52385849d7e0342eb6060bf43
j2scode <j2sdatalab@gmail.com>
2017-11-23 15:16:56 -0500

    Final model 2

--- R/prediction.R
+++ R/prediction.R
@@ -10,19 +10,28 @@
 #' @param case Data frame containing single case to predict
 #' @param unit Character string indicating the unit for the value being predicted (w/o log)
 #' @param conf Numeric indicator (0 to 1) of the confidence level for the prediction interval.
+#' @param actual Logical indicating whether the dependent variable is actual or predicted.
 #'
 #' @return
 #'
 #' @author John James, \email{jjames@@datasciencesalon.org}
 #' @family movies functions
 #' @export
-prediction <- function(mod, mName, case, unit, conf = .90) {
+prediction <- function(mod, mName, case, unit, conf = .90, actual = TRUE) {
 
   # Get summmary statistics for model
   s <- broom::glance(mod)
+  a <- broom::tidy(anova(mod))
 
   # Extract name of response variable
   yVar <- mod$terms[[2]]
+
+  # Set predictor variable
+  if (nrow(a) == 2) {
+    predictor <- a$term[1]
+  } else {
+    predictor <- paste0(nrow(a) - 1, " predictors")
+  }
 
   # Perform prediction and summarize results
   p <- predict(mod, case, se.fit = TRUE, scale = NULL, df = Inf,
@@ -32,12 +41,22 @@ prediction <- function(mod, mName, case, unit, conf = .90) {
   y <- case[,(colnames(case) == yVar)]
   yLin <- 2^y
 
+  # Set indicator if actual was within 95% CI of prediction
+  if (y > p$fit[2] & y < p$fit[3]) {
+    ci <- "within"
+  } else {
+    ci <- "outside"
+  }
+
+  # Compute prediction on linear scale
   yHat <- p$fit[1]
   yHatLin <- 2^yHat
 
+  # Compute Errors
   e <-  y - yHat
   eLin <- yLin - yHatLin
 
+  # Compute percent errors
   pe <- abs(e / y * 100)
   peLin <- abs(yLin - yHatLin) / yLin * 100
 
@@ -49,9 +68,10 @@ prediction <- function(mod, mName, case, unit, conf = .90) {
                            ifelse(s$p.value < .01, ".01",
                                   ifelse(s$p.value < .05, ".05"))),
                     ") was used to predict the ", yVar,  " of the film '",
-                    case$title, "'. The actual value was ", round(y, 2), " log ",
-                    unit, ". ", "The model predicted ", round(yHat, 2), " log ",
-                    unit, " with a 95% prediction interval CI[",
+                    case$title, "' based upon ", predictor,
+                    ". The actual value of ", yVar, " was ", round(y, 2),
+                    " log ", unit, ". ", "The model predicted ", round(yHat, 2), " log ",
+                    unit, ", ", ci, " the 95% prediction interval CI[",
                     round(p$fit[2], 2), ",", round(p$fit[3], 2),"] log ", unit,
                     ", equating to an ", overUnder, " prediction of ",
                     round(pe, 2), "%. ")
